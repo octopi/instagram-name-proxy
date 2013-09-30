639008fc5e32eeb2033ad671d3892ec7f12f95d2
David Hu <octopie@gmail.com>
2013-09-29 21:53:08 -0400

    Add support for max_id, better err handling

--- server.js
+++ server.js
@@ -17,12 +17,18 @@ app.get('/', function(req, res) {
 
 app.get('/:username', function(req, res) {
   var igUsername = req.params.username,
+  igMaxId = req.query.max_id,
   igUserId;
 
   async.series([
-    // do search first
+    // find the user
     function(callback) {
       ig.user_search(igUsername, {count: 1}, function(err, users, limit) {
+        if (err) {
+          callback(true, { 'error': err });
+          return;
+        }
+
         if (users.length > 0) {
           igUserId = users[0].id;
           callback();
@@ -34,10 +40,20 @@ app.get('/:username', function(req, res) {
       });
     },
 
-    // get this user's last 20 images
+    // grab da images
     function(callback) {
-      ig.user_media_recent(igUserId, {count: 20}, function(err, media, pagination, limit) {
-        callback(null, { 'media': media });
+      ig.user_media_recent(igUserId,
+        {count: 20, max_id: igMaxId},
+        function(err, media, pagination, limit) {
+          if (err) {
+            callback(true, { 'error': err });
+            return;
+          }
+
+          if (pagination)
+            callback(null, { 'media': media, 'next_max_id': pagination.next_max_id });
+          else
+            callback(null, { 'media': media });
       });
     }
   ],
